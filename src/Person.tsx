d50e52bcc8970c0e550146521d31ef15b4564ae7
daho2353 <daho2353@colorado.edu>
2024-11-05 13:23:17 -0800

    added tutorial information with basic functions in the Person component.

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export interface Person{
     name: string;
     age: number;
@@ -5,12 +7,33 @@ export interface Person{
 } //type can be used anywhere in project when exported 
  
 export const Person = (props: Person)=> {
+    const [isShowInfo, setShowInfo] = useState<boolean|null>(false);
+    const [bio, setBio] = useState<string | null>("me");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { //OnChange values have special React types that handle input cases
+        setBio(event.target.value)
+    };
+
+    const formSubmit = (event: React.FormEvent<HTMLFormElement>) => { //this is how you would type a form submit
+
+    }
+
+    const toggleInfo = () => {
+        setShowInfo((prev) => !prev); //take the value of prev and return the opposite, toggle boolean
+    };
+
     return (
-        <div> 
-            <p>Name: {props.name} </p>
-            <p>Age: {props.age}</p>
-            <p>Married: {props.isMarried ? "is married" : "is single"}</p>
-            
+        <div>
+            {isShowInfo && (
+                <>
+                    <p>Name: {props.name} </p>
+                    <p>Age: {props.age}</p>
+                    <p>Married: {props.isMarried ? "is married" : "is single"}</p>
+                </>
+             )}
+             <p>{props.name}'s Bio: {!bio ? "no bio available" : bio}</p>
+             <input onChange={handleChange}/>
+             <button onClick={toggleInfo}> Toggle </button>
         </div>
     );
 };
